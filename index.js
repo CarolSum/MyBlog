2c8c9d6e09b1b9b442fd15f828c6a26a946ef65f
CarolSum <905965287@qq.com>
2017-11-23 09:48:25 +0800

    完成简单页面设计

--- index.js
+++ index.js
@@ -5,13 +5,14 @@ const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 const config = require('config-lite')(__dirname)
 const routes = require('./routes')
+const pkg = require('./package')
 
 const app = express()
 
 // 设置模板目录
 app.set('views', path.join(__dirname, 'views'))
-// 设置模板引擎为 ejs
-app.set('view engine', 'ejs')
+// 设置模板引擎为 jade
+app.set('view engine', 'jade')
 
 // 设置静态文件目录
 app.use(express.static(path.join(__dirname, 'public')))
@@ -30,6 +31,20 @@ app.use(session({
 }))
 // flash 中间件，用来显示通知
 app.use(flash())
+
+//设置模板全局变量
+app.locals.blog = {
+  title: pkg.name,
+  description: pkg.description
+}
+
+//添加模板必须的三个变量
+app.use(function(req, res, next){
+  res.locals.user = req.session.user;
+  res.locals.success = req.flash('success').toString();
+  res.locals.error = req.flash('error').toString();
+  next();
+})
 
 // 路由
 routes(app)
